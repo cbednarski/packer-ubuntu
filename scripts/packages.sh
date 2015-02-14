34f0d424f9dc3e2290c1917d2b7e3c4ccfe7c0a1
Chris Bednarski <banzaimonkey@gmail.com>
2015-02-13 23:42:00 -0800

    Update packages when building the VM so we don't have to do it each time we spin up a box

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,3 +1,5 @@
+apt-get clean
 apt-get update
 apt-get upgrade -y
-apt-get install -y vim tree
+apt-get install -y linux-generic linux-headers-generic linux-headers-server linux-image-generic linux-server
+# apt-get install -y vim tree
--- scripts/vm_cleanup.sh
+++ scripts/vm_cleanup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+apt-get remove -y linux-headers-generic linux-headers-server build-essential
+apt-get autoremove -y
+apt-get clean
+
 # Removing leftover leases and persistent rules
 echo "cleaning up dhcp leases"
 rm /var/lib/dhcp/*
