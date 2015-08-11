5afefab5f36779b8266b80106a7baf913074d4ad
Chris Bednarski <banzaimonkey@gmail.com>
2015-08-11 16:46:53 -0700

    Fix invalid code in the dev-packages script

--- scripts/dev-packages.sh
+++ scripts/dev-packages.sh
@@ -2,11 +2,10 @@
 apt-get install -y wget curl vim git mercurial bzr tree python python-pip htop nmap
 
 # Install ifs
+pip install -U setuptools pip
 pip install ifs
 
 # Install hostess
-wget https://github.com/cbednarski/hostess/releases/download/v0.1.0/hostess_linux_amd64
+wget -q https://github.com/cbednarski/hostess/releases/download/v0.1.0/hostess_linux_amd64
 chmod +x hostess_linux_amd64
 mv hostess_linux_amd64 /usr/local/bin/hostess
-
---header "Cookie: oraclelicense=accept-securebackup-cookie"
