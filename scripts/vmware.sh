652052fdee59c688c316aae346bdccc7cb4dc94d
Alvaro Miranda <kikitux@gmail.com>
2015-10-17 20:44:45 +1300

    Force install of VmwareTools
    `--force-install` is required, otherwise will suggest go for open-vm-tools.
    
    This change has been tested, and proven that works.

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -6,7 +6,7 @@ cd /tmp
 mkdir -p /mnt/cdrom
 mount -o loop ~/linux.iso /mnt/cdrom
 tar zxf /mnt/cdrom/VMwareTools-*.tar.gz -C /tmp/
-/tmp/vmware-tools-distrib/vmware-install.pl -d
+/tmp/vmware-tools-distrib/vmware-install.pl -d --force-install
 rm /home/vagrant/linux.iso
 umount /mnt/cdrom
 
