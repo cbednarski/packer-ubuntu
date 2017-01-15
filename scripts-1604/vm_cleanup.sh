f06270685095344c85b952777366939aa6ee629e
Chris Bednarski <banzaimonkey@gmail.com>
2017-01-15 15:40:39 -0800

    Change VMware to always use PCI-e 32 for the NIC; cleanup linux.iso

--- scripts-1604/vm_cleanup.sh
+++ scripts-1604/vm_cleanup.sh
@@ -6,13 +6,8 @@ apt-get clean
 echo "cleaning up dhcp leases"
 rm /var/lib/dhcp/*
 
-# When booting with Vagrant / VMware the PCI slot is changed from 33 to 32.
-# Instead of eth0 the interface is now called ens33 to mach the PCI slot, so we
-# need to change the networking scripts to enable the correct interface.
-#
-# NOTE: After the machine is rebooted Packer will not be able to reconnect
-# (Vagrant will be able to) so make sure this is done in your final provisioner.
-sed -i "s/ens33/ens32/g" /etc/network/interfaces
+# Cleanup tools iso
+rm -f /home/vagrant/linux.iso
 
 # Zero out the free space to save space in the final image:
 echo "Zeroing device to make space..."
