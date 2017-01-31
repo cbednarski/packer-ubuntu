b2abe4bbe98d432bb9450dfe0d1e52c77796580f
Chris Bednarski <banzaimonkey@gmail.com>
2017-01-31 14:36:31 -0800

    Put this back in because apparently we can't validate github's cert on linux

--- scripts-1604/vagrant.sh
+++ scripts-1604/vagrant.sh
@@ -11,7 +11,7 @@ usermod -a -G sudo vagrant
 mkdir /home/vagrant/.ssh
 chmod 700 /home/vagrant/.ssh
 cd /home/vagrant/.ssh
-wget 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
+wget --no-check-certificate 'https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub' -O authorized_keys
 chmod 600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
